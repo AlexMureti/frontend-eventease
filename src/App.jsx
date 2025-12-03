5be3576e87c0afcbb763d2e64368a5889f584b47
Hillary Tanui <hillarytanui80@gmail.com>
2025-12-03 07:18:56 +0300

    added gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   
   return (
     <>
+    <h1 className="text-blue-500">eventease</h1>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
   ],
   server: {
     port: 3000
-  }
+  },
+  base: "/frontend-eventease/"
   
 })
